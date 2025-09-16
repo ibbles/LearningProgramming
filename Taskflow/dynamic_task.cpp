f230070aa892f0e4fa2b1eab9404e3391264aac2
Martin Nilsson <ibbles@hotmail.com>
2025-09-16 22:09:22 +0200

    Fix indentation in dynamic_task.cpp

--- Taskflow/dynamic_task.cpp
+++ Taskflow/dynamic_task.cpp
@@ -4,27 +4,28 @@
 // Standard library includes.
 #include <iostream>
 
+// Written in 'input_task', read in 'dynamic_task'.
 int global_state {0};
 
 void input_task()
 {
 	std::cout << "How many dynamic tasks? ";
-    std::cin >> global_state;
+	std::cin >> global_state;
 }
 
 // This task is not scheduled in main, instead it is
 // scheduled by create_dynamic_tasks.
 void dynamic_task()
 {
-    std::cout << "Dynamic task.\n";
+	std::cout << "Dynamic task.\n";
 }
 
 void create_dynamic_tasks(tf::Subflow& subflow)
 {
-    for (int i = 0; i < global_state; ++i)
-    {
-        subflow.emplace(dynamic_task);
-    }
+	for (int i = 0; i < global_state; ++i)
+	{
+		subflow.emplace(dynamic_task);
+	}
 }
 
 int main()
@@ -32,8 +33,8 @@ int main()
 	tf::Executor executor;
 	tf::Taskflow taskflow;
 	tf::Task input = taskflow.emplace(input_task);
-    tf::Task dynamic = taskflow.emplace(create_dynamic_tasks);
-    input.precede(dynamic);
+	tf::Task dynamic = taskflow.emplace(create_dynamic_tasks);
+	input.precede(dynamic);
 	executor.run(taskflow).wait();
 	return 0;
 }
