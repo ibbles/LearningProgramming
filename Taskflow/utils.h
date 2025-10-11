5b023984d6723f15cde65e1bea49a45230fabe83
Martin Nilsson <ibbles@hotmail.com>
2025-10-11 15:31:30 +0200

    Add printTaskFlow to utils.h

--- Taskflow/utils.h
+++ Taskflow/utils.h
@@ -7,6 +7,19 @@
 #include <filesystem>
 #include <fstream>
 
+inline void printTaskflow(tf::Taskflow& taskflow)
+{
+	for (auto& node_it : taskflow.graph())
+	{
+		auto* node = node_it.get();
+		std::cout << "Node " << node->name() << ":\n";
+		std::cout << "  Predeciessors: " << node->num_predecessors() << '\n';
+		std::cout << "  Successors: " << node->num_successors() << '\n';
+		std::cout << "  Strong dependencies: " << node->num_strong_dependencies() << '\n';
+		std::cout << "  Weak dependencies: " << node->num_weak_dependencies() << "\n";
+	}
+}
+
 inline void dumpToFile(tf::Taskflow& taskflow, std::filesystem::path path)
 {
 	errno = 0;
